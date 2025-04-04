3030850a3e45d04d219b90ccf075ed7a70e12548
leftrana <leftrana@student.21-school.ru>
2025-04-04 11:56:56 +0300

    add fan in

---
+++ cmd/fan_in/mian.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println(start)
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	channel3 := make(chan string)
+	go func() {
+		defer func() {
+			close(channel1)
+			close(channel2)
+			close(channel3)
+		}()
+		// add data to channels
+		datasize := 100
+		for i := range datasize {
+			channel1 <- "channel - 1 " + strconv.Itoa(i)
+			channel2 <- "channel - 2 " + strconv.Itoa(i+1)
+			channel3 <- "channel - 3 " + strconv.Itoa(i+3)
+			time.Sleep(300)
+		}
+	}()
+
+	// read data from channels
+	for value := range MergeChannels(channel1, channel2, channel3) {
+		fmt.Println(value)
+	}
+	fmt.Println(time.Since(start))
+}
+
+func MergeChannels[T any](channels ...chan T) <-chan T {
+	var wg sync.WaitGroup
+	wg.Add(len(channels))
+	mergedChannel := make(chan T)
+
+	for _, channel := range channels {
+		go func() {
+			defer wg.Done()
+			for value := range channel {
+				mergedChannel <- value
+			}
+		}()
+
+	}
+	go func() {
+		wg.Wait()
+		close(mergedChannel)
+	}()
+
+	return mergedChannel
+}
